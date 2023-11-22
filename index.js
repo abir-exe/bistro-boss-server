9505c9524b3ed402f3f525be0be3a28c92dcfe0e
Abir <abirmahmud221@gmail.com>
2023-11-22 19:54:18 +0600

    68.6

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -49,7 +50,27 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
 
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+    //menu related api
     app.get('/menu', async(req, res) => {
         const result = await  menuCollection.find().toArray();
         res.send(result);
